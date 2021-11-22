28a4e543a6c96d7b2ed7d290f6cdb7d020101380
peredz <94736048+peredz@users.noreply.github.com>
2021-11-22 19:57:54 +1000

    o

---
+++ main.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
+from PyQt5 import uic
+from sys import argv, exit
+from random import choice
+
+
+class CyrcleVashka(QWidget):
+    def __init__(self):
+        uic.loadUi('disg.ui', self)
+        super().__init__()
+        self.setup()
+
+    def setup(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    app = QApplication(argv)
+    ex = CyrcleVashka()
+    ex.show()
+    exit(app.exec())
